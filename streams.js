67196d00a2efb47644a2be89bd9a18bf01eb3c9d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-07 09:23:34 +0530

    added fine grade permissions

--- streams.js
+++ streams.js
@@ -1,11 +1,31 @@
 LineStream = new Meteor.Stream('lines');
 
 if(Meteor.isServer) {
-  LineStream.permissions.read(function() {
-    return true;
+  //map subscriptionId and the padId he is listening to
+  var subscriptionPadsMap = {};
+  LineStream.on('pad', function(padId) {
+    var subscriptionId = this.subscriptionId;
+    subscriptionPadsMap[subscriptionId] = padId;
+
+    this.onDisconnect = function() {
+      subscriptionPadsMap[subscriptionId] = undefined;
+    };
   });
 
-  LineStream.permissions.write(function() {
+  LineStream.permissions.read(function(event) {
+    //getting padId from the event
+    var matched = event.match(/(.*):/);
+    if(matched) {
+      var padId = matched[1];
+      //only allow events with padId where subscription is interestedIn
+      return subscriptionPadsMap[this.subscriptionId] == padId;
+    } else {
+      //only allows events with padId to read from the stream
+      return false;
+    }
+  }, false);
+
+  LineStream.permissions.write(function(event) {
     return true;
   });
 }
